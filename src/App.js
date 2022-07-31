71b668a5f9360972330c0fc4472d66b3d7433cd4
Morne <morneventer.mv@gmail.com>
2022-07-31 18:03:12 +0200

    fix: sidebar and compielr options

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import "App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import { Sidebar } from "components/sidebar/sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppRoutes } from "shared/routes";
 import { HomePage } from "./components/homePage/homePage";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<HomePage />}>
-					<Route path='home' element={<HomePage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<>
+			<Sidebar />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />}>
+						<Route path={AppRoutes.Home} element={<HomePage />} />
+						<Route path={AppRoutes.Contact} element={<HomePage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
