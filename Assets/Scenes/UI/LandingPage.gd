de80619b0d5e3d3c01f66288c059559c1f9edbe8
Morne <morneventer.mv@gmail.com>
2022-08-04 10:02:56 +0200

    add mobile

--- Assets/Scenes/UI/LandingPage.gd
+++ Assets/Scenes/UI/LandingPage.gd
@@ -3,11 +3,16 @@ extends Control
 var white = Color(1,1,1)
 var black = Color(0,0,0)
 
+signal aspect_change(is_mobile)
+
 onready var anim = $AnimationPlayer
+
+var is_mobile = false
 
 func _ready() -> void:
 	_light_mode()
 	anim.current_animation = "show_home"
+	_check_mobile()
 
 func _dark_mode() -> void:
 	$Header.modulate = black
@@ -29,16 +34,20 @@ func _on_HomeButton_mouse_entered() -> void:
 	anim.current_animation = "show_home"
 
 func _on_InfoButton_mouse_entered() -> void:
-	anim.current_animation = "show_info"
+	if !is_mobile:
+		anim.current_animation = "show_info"
 
 func _on_SkillsButton_mouse_entered() -> void:
-	anim.current_animation = "show_skills"
+	if !is_mobile:
+		anim.current_animation = "show_skills"
 
 func _on_ProjectButton_mouse_entered() -> void:
-	anim.current_animation = "show_project"
+	if !is_mobile:
+		anim.current_animation = "show_project"
 
 func _on_ContactButton_mouse_entered() -> void:
-	anim.current_animation = "show_contact"
+	if !is_mobile:
+		anim.current_animation = "show_contact"
 
 func _on_EmberButton_pressed() -> void:
 	OS.shell_open("https://store.steampowered.com/app/1848860/Ember_Crusade_IV/")
@@ -60,7 +69,80 @@ func _on_EmailButton_pressed() -> void:
 
 func _copy_email():
 	$ContactCard/AnimationPlayer.current_animation = "copied"
+	$Mobile/ContactCard/AnimationPlayer.current_animation = "copied"
 	OS.set_clipboard("morneventer.mv@gmail.com")
 
 func _on_Github_pressed() -> void:
 	OS.shell_open("https://github.com/MorneVenter")
+
+func _check_mobile():
+	var viewport = get_viewport().size
+	if viewport.y > viewport.x:
+		_set_mobile()
+		emit_signal("aspect_change", true)
+		is_mobile = true
+	else:
+		_set_desktop()
+		emit_signal("aspect_change", false)
+		is_mobile = false
+
+func _set_mobile():
+	$AnimationPlayer.current_animation = "show_home"
+	_show_home_mobile()
+
+func _set_desktop():
+	$AnimationPlayer.current_animation = "show_home"
+	_show_home_mobile()
+
+func _on_LandingPage_resized() -> void:
+	_check_mobile()
+
+func _show_info_mobile():
+	$Mobile/InfoCard.visible = true
+	$Mobile/SkillsCard.visible = false
+	$Mobile/ProjectCard.visible = false
+	$Mobile/ContactCard.visible = false
+
+func _show_skills_mobile():
+	$Mobile/InfoCard.visible = false
+	$Mobile/SkillsCard.visible = true
+	$Mobile/ProjectCard.visible = false
+	$Mobile/ContactCard.visible = false
+
+func _show_projects_mobile():
+	$Mobile/InfoCard.visible = false
+	$Mobile/SkillsCard.visible = false
+	$Mobile/ProjectCard.visible = true
+	$Mobile/ContactCard.visible = false
+
+func _show_contact_mobile():
+	$Mobile/InfoCard.visible = false
+	$Mobile/SkillsCard.visible = false
+	$Mobile/ProjectCard.visible = false
+	$Mobile/ContactCard.visible = true
+
+func _show_home_mobile():
+	$Mobile/InfoCard.visible = false
+	$Mobile/SkillsCard.visible = false
+	$Mobile/ProjectCard.visible = false
+	$Mobile/ContactCard.visible = false
+
+func _on_HomeButton_pressed() -> void:
+	if is_mobile:
+		_show_home_mobile()
+
+func _on_InfoButton_pressed() -> void:
+	if is_mobile:
+		_show_info_mobile()
+
+func _on_SkillsButton_pressed() -> void:
+	if is_mobile:
+		_show_skills_mobile()
+
+func _on_ProjectButton_pressed() -> void:
+	if is_mobile:
+		_show_projects_mobile()
+
+func _on_ContactButton_pressed() -> void:
+	if is_mobile:
+		_show_contact_mobile()
